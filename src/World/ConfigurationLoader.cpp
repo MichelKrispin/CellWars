811e70f2fb404206c5437369a7e15dd539e12dee
MichelKrispin <michel.krispin@gmail.com>
2019-10-15 20:25:30 +0200

    Created dynamically loading class

---
+++ src/World/ConfigurationLoader.cpp
@@ -0,0 +1,113 @@
+#include "ConfigurationLoader.h"
+#include <fstream>
+#include <iostream>
+
+// Default values
+ConfigurationLoader::ConfigurationLoader()
+    : _MaxCountPerField(100),
+      _WindowSize(800),
+      _GridSize(50),
+      _MaxTurnCount(1000),
+      _TurnDurationInMs(50),
+      _BackgroundColor(sf::Color::Black),
+      _FontPath("arial.ttf"),
+      _PauseOnStartup(false)
+{
+    std::string Line; // Buffer for each line
+    // Open up the file
+    std::ifstream ConfigurationFile("config.txt");
+    if (!ConfigurationFile.is_open()) // If file is not open return
+    {
+        std::cout << "Could not find the config.txt - Initializing with default values\n";
+        return;
+    }
+
+    // Read the input line by line
+    while (std::getline(ConfigurationFile, Line))
+    {
+        if (Line.rfind("//", 0) == 0)
+            continue;
+        if (Line.rfind("MAX_COUNT_PER_FIELD", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            _MaxCountPerField = static_cast<unsigned int>(std::stoi(Line, nullptr, 0));
+        }
+        else if (Line.rfind("WINDOW_SIZE", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            _WindowSize = static_cast<unsigned int>(std::stoi(Line, nullptr, 0));
+        }
+        else if (Line.rfind("GRID_SIZE", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            _GridSize = static_cast<unsigned int>(std::stoi(Line, nullptr, 0));
+        }
+        else if (Line.rfind("TURN_DURATION_IN_MS", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            _TurnDurationInMs = static_cast<unsigned int>(std::stoi(Line, nullptr, 0));
+        }
+        else if (Line.rfind("BACKGROUND_COLOR", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            if (Line == "White")
+                _BackgroundColor = sf::Color::White;
+            else
+                _BackgroundColor = sf::Color::Black;
+        }
+        else if (Line.rfind("FONT_PATH", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            _FontPath = Line;
+        }
+        else if (Line.rfind("FONT_PATH", 0) == 0)
+        {
+            std::getline(ConfigurationFile, Line);
+            if (Line == "false")
+                _PauseOnStartup = false;
+            else
+                _PauseOnStartup =true;
+        }
+    }
+}
+
+const unsigned int& ConfigurationLoader::GetMaxCountPerField() const
+{
+    return _MaxTurnCount;
+}
+
+const unsigned int& ConfigurationLoader::GetWindowSize() const
+{
+    return _WindowSize;
+}
+
+const unsigned int& ConfigurationLoader::GetGridSize() const
+{
+    return _GridSize;
+}
+
+const unsigned int& ConfigurationLoader::GetMaxTurnCount() const
+{
+    return _MaxTurnCount;
+}
+
+const unsigned int& ConfigurationLoader::GetTurnDurationInMs() const
+{
+    return _TurnDurationInMs;
+}
+
+const sf::Color& ConfigurationLoader::GetBackgroundColor() const
+{
+    return _BackgroundColor;
+}
+
+const std::string& ConfigurationLoader::GetFontPath() const
+{
+    return _FontPath;
+}
+
+const bool& ConfigurationLoader::GetPauseOnStartup() const
+{
+    return _PauseOnStartup;
+}
+
